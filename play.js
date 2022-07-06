3877528c5f34a6a486e1560eb58e2d630f32fced
jadenlee0519 <jadenlee0519@gmail.com>
2022-07-06 16:01:07 -0700

    add setupinput&gandleuserinput

--- play.js
+++ play.js
@@ -19,6 +19,23 @@ const connect = function () {
   return conn;
 };
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = process.stdin.on ("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+    return stdin;
+};
+
+
+
 console.log("Connecting ...");
 connect();
 
+setupInput();
